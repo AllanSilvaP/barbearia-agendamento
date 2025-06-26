daa1eab74b8a8af52e1e1675fb97e54ef567ab9b
AllanSilvaP <allan.dasilvap2005@gmail.com>
2025-06-26 12:53:33 -0300

    Ajuste page cadastro -> Terminar isso

--- barbeiro-app/src/app/cadastro/page.js
+++ barbeiro-app/src/app/cadastro/page.js
@@ -1,31 +1,45 @@
+import {useState} from 'react'
 import Navbar from "@/components/Navbar";
 
-export default function LoginPage() {
+export default function CadastroPage() {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [erro, setErro] = useState("")
     return (
         <>
             <Navbar />
             <main className="min-h-screen flex flex-col items-center justify-center bg-[#D7CCC8]">
                 <h1 className="font-bold text-3xl mb-6">Cadastro</h1>
-                <form className="bg-[#808080] shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm">
+                <form 
+                className="bg-[#808080] shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm"
+                onSubmit={ha} // TODO HANDLER DAQUI
+                >
+                    <div className="mb-4">
+                        <input
+                            type="text"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            placeholder="Nome"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                        ></input>
+                    </div>
                     <div className="mb-4">
                         <input
                             type="text"
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                             placeholder="Email"
-                        ></input>
-                    </div>
-                    <div className="mb-4">
-                        <input
-                            type="password"
-                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            placeholder="Senha"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         ></input>
                     </div>
                     <div className="mb-6">
                         <input
                             type="password"
                             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            placeholder="Confirme sua senha"
+                            placeholder="Senha"
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}
                         ></input>
                     </div>
 
